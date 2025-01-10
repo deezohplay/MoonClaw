73f99bf573eb41c4813219672b6022cad5ffbab2
Rubangakene Inno Joseph <57228148+deezohplay@users.noreply.github.com>
2025-01-10 19:37:53 +0300

    Update GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,18 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    private float startTime;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        startTime = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float t = Time.time - startTime;
     }
 }
