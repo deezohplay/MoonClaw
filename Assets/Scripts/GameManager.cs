b013ff61d96da91b4df2f79aa1f48de268cbb463
Rubangakene Inno Joseph <57228148+deezohplay@users.noreply.github.com>
2025-01-12 12:42:20 +0300

    Update GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,7 @@ using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    PlayerController playerController;
     private float startTime;
     public TextMeshProUGUI timerText;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -15,9 +16,11 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float t = Time.time - startTime;
-        string minutes = ((int) t / 60).ToString();
-        string seconds = (t % 60).ToString("f2");
-        timerText.text = minutes + ":" + seconds;
+        if(playerController.startGame == true){
+            float t = Time.time - startTime;
+            string minutes = ((int) t / 60).ToString();
+            string seconds = (t % 60).ToString("f2");
+            timerText.text = minutes + ":" + seconds;
+        }
     }
 }
